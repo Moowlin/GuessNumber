47e5f82785f3bdde87356e2ec7526d33fcd735b7
Olga Nizhegorodtseva Virkraft <moowlin@mail.ru>
2021-12-10 12:27:33 +0300

    добавлен режим игры №2

--- GuessNumber.py
+++ GuessNumber.py
@@ -28,6 +28,7 @@ def NumRangeValidation():
                 break
         except ValueError:
             print('Введите целое положительное число')
+
 # ================================================= def GUESS_1 ========================================================
 def guess1():
     '''Пользователь угадывает загаданное программой число'''
@@ -67,6 +68,51 @@ def guess1():
             print('Вы указали недопустимый ответ')
 # ================================================= def GUESS_2 ========================================================
 
+def guess2():
+    '''Программа угадывает загаданное пользователем число'''
+
+    NumRange = NumRangeValidation()       # Определяем диапазон чисел, проверяем корректность ввода числа пользователем
+
+    print('Правила:',
+          f'1. Загадайте число в диапазоне [1; {NumRange}]',
+          '2. Программа будет выводить варианты ответа',
+          '3. Вам необходимо будет ответить загаданное больше или меньше предложенного варианта', sep="\n")
+
+    input('Загадайте число и нажмите Enter для продолжения ')
+
+    count = 0                            # Заводим счетчик попыток угадать
+    # Цикл угадываения числа:
+    NumbersRange = [i for i in range(1, NumRange + 1)]  # Получаем список
+
+    mid = len(NumbersRange) // 2
+    low = 0
+    high = len(NumbersRange) - 1
+    answer = ''
+
+    while answer not in answersYES and low <= high:
+        count += 1
+        print(f'мой ответ {NumbersRange[mid]}')
+
+        while (answer not in answersNO) and (answer not in answersYES):
+            answer = input('Угадал? (yes/no) ')
+
+        if answer in answersYES:
+            print('УРА')
+            print(f'Я угадал за {count} попыток')
+            if count == ceil(log(NumRange, 2)): print("Хороший результат")
+            elif count < ceil(log(NumRange, 2)): print("Отличный результат")
+        else:
+            while (answer not in answersGreater) and (answer not in answersLess):
+                answer = input('Ваше число больше (>) или меньше (<)? ')
+
+        if answer in answersGreater:
+            low = mid + 1
+        if answer in answersLess:
+            high = mid - 1
+        mid = (low + high) // 2
+
+    if low > high:
+        print('А вы точно загадали число?')
 
 # ================================================= def CHOICE =========================================================
 def choice():
@@ -88,10 +134,9 @@ while answer not in (answersYES + answersNO + answersExit):
     if answer == '1':       # если параметр 1 - запускам guess1
         guess1()
     elif answer == '2':     # если параметр 2 - запускаем guess2
-        print("режим №2 еще не реализован =)")
-#        guess2()
+        guess2()
 
-    answer = input("Хотите сыграть еще раз? y/n ").strip().lower()
+    answer = input("Хотите сыграть еще раз? yes/no ").strip().lower()
     if answer in answersYES:
         answer = choice()
     elif answer in answersNO:
